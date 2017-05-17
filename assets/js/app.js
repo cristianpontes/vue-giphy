1bee08d7ef0d18c6209efee26be1397e2477e579
Cristian Pontes <hsilencee@gmail.com>
2017-05-17 16:04:40 +0100

    first commit, vuejs setup

---
+++ assets/js/app.js
@@ -0,0 +1,13 @@
+var apiEndPoint = 'http://api.giphy.com/v1/gifs/';
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        search: '',
+        results: [],
+        viewer: false
+    },
+    methods: {
+
+    }
+});
