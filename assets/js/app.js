73b2ac708095f1c0c67ed19933db2653990db2dc
Cristian Pontes <hsilencee@gmail.com>
2017-05-17 17:15:13 +0100

    Giphy API and gifs visualization

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,25 @@
-var apiEndPoint = 'http://api.giphy.com/v1/gifs/';
+var apiEndPoint = 'http://api.giphy.com/v1/gifs/search';
+var publicKey = 'dc6zaTOxFJmzC';
 
-var app = new Vue({
+new Vue({
     el: '#app',
     data: {
-        search: '',
+        query: '',
         results: [],
-        viewer: false
+        current_gif: false
     },
     methods: {
-
+        searchGIFs: function () {
+            var xhr = new XMLHttpRequest();
+            var self = this;
+            xhr.open('GET', apiEndPoint + '?q=' + self.query.split(' ').join('+') + '&api_key=' + publicKey);
+            xhr.onload = function () {
+                self.results = JSON.parse(xhr.responseText).data;
+            };
+            xhr.send();
+        },
+        viewGIF: function (gif) {
+            this.current_gif = gif.images.original.url;
+        }
     }
 });
